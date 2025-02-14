df52aac156802e8921a04a5d02869f9d22c39ca5
Sinclert Pérez <sinclert.perez@canonical.com>
2025-02-14 10:58:54 +0100

    Fix PostgreSQL user and group definitions

--- tests/clients/psql/test_postgres_client.py
+++ tests/clients/psql/test_postgres_client.py
@@ -25,20 +25,39 @@ class TestDefaultPostgresClient:
             auto_commit=False,
         )
 
-        client.create_user("user_1", login=True)
-        client.create_user("user_2", login=True)
-        client.create_user("user_3", login=False)
+        client.create_user("user_1")
+        client.create_user("user_2")
+        client.create_user("user_3")
 
-        client.create_group("group_1", login=True)
-        client.create_group("group_2", login=True)
-        client.create_group("group_3", login=False)
+        client.create_group("group_1")
+        client.create_group("group_2")
+        client.create_group("group_3")
 
         client.grant_group_memberships(["group_1"], ["user_1", "user_2"])
         client.grant_group_memberships(["group_2"], ["user_2", "user_3"])
+        client.grant_group_memberships(["group_3"], ["group_1"])
 
         yield client
 
         client.close()
+
+    def test_create_user(self, client: DefaultPostgresClient):
+        """Test the create_user functionality."""
+        client.create_user("john")
+
+        users = client.search_users()
+        users = list(users)
+
+        assert "john" in users
+
+    def test_create_group(self, client: DefaultPostgresClient):
+        """Test the create_user functionality."""
+        client.create_group("group_rw")
+
+        groups = client.search_groups()
+        groups = list(groups)
+
+        assert "group_rw" in groups
 
     def test_search_users(self, client: DefaultPostgresClient):
         """Test the search_users functionality."""
@@ -47,7 +66,7 @@ class TestDefaultPostgresClient:
 
         assert "user_1" in users
         assert "user_2" in users
-        assert "user_3" not in users
+        assert "user_3" in users
 
     def test_search_groups(self, client: DefaultPostgresClient):
         """Test the search_groups functionality."""
@@ -56,7 +75,7 @@ class TestDefaultPostgresClient:
 
         assert "group_1" in groups
         assert "group_2" in groups
-        assert "group_3" not in groups
+        assert "group_3" in groups
 
     def test_search_group_memberships(self, client: DefaultPostgresClient):
         """Test the search_group_memberships functionality."""
@@ -65,4 +84,4 @@ class TestDefaultPostgresClient:
 
         assert GroupMembers("group_1", ["user_1", "user_2"]) in memberships
         assert GroupMembers("group_2", ["user_2", "user_3"]) in memberships
-        assert GroupMembers("group_3", []) not in memberships
+        assert GroupMembers("group_3", ["group_1"]) not in memberships
